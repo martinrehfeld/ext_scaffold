110641162ef1df5a50fdf21d2100264d2bcc9743
martinrehfeld <martinrehfeld@ff3fcdfe-c7f7-449c-8fb0-feba545ac656>
2008-01-06 11:29:33 +0000

    register ext_json MIME type on init
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rug-b/ext_scaffold@53 ff3fcdfe-c7f7-449c-8fb0-feba545ac656

--- init.rb
+++ init.rb
@@ -4,3 +4,6 @@ Dir[File.join("#{File.dirname(__FILE__)}", 'lib', 'ext_scaffold_core_extensions'
   base_module = f.sub(/(.*ext_scaffold_core_extensions.)(.*)\.rb/,'\2').classify.constantize
   base_module.class_eval { include extension_module }
 end
+
+# Register MIME type
+Mime::Type.register_alias "text/javascript", :ext_json
