0b4796a5f0c2156f9afa79e42cd841a99b1e5268
martinrehfeld <martinrehfeld@ff3fcdfe-c7f7-449c-8fb0-feba545ac656>
2008-01-06 20:15:14 +0000

    fixed post-install message
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rug-b/ext_scaffold@64 ff3fcdfe-c7f7-449c-8fb0-feba545ac656

--- install.rb
+++ install.rb
@@ -4,7 +4,7 @@ You now need to download the Ext Javascript framework from
 
 http://extjs.com/download
 
-and unzip it into "#{RAILS_ROOT}/public/ext" if you have not done so yety.
+and unzip it into "#{RAILS_ROOT}/public/ext" if you have not done so yet.
 Ext_scaffold was tested against version 2.0 of the Ext framework, available via
 
 http://extjs.com/deploy/ext-2.0.zip
