7d9325e3d4061db3c4b9d48f1084dfd5d985577e
martinrehfeld <martinrehfeld@ff3fcdfe-c7f7-449c-8fb0-feba545ac656>
2008-01-07 22:06:07 +0000

    fixed generated functional test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rug-b/ext_scaffold@71 ff3fcdfe-c7f7-449c-8fb0-feba545ac656

--- generators/ext_scaffold/templates/functional_test.rb
+++ generators/ext_scaffold/templates/functional_test.rb
@@ -1,8 +1,14 @@
 require File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../test_helper'
 
+# make sure the secret for request forgery protection is set (views will
+# explicitly use the form_authenticity_token method which will fail otherwise)
+<%= controller_class_name %>Controller.request_forgery_protection_options[:secret] = 'test_secret'
+
 class <%= controller_class_name %>ControllerTest < ActionController::TestCase
   def test_should_get_index
     get :index
+    assert_response :success
+    get :index, :format => 'ext_json'
     assert_response :success
     assert_not_nil assigns(:<%= table_name %>)
   end
@@ -14,10 +20,11 @@ class <%= controller_class_name %>ControllerTest < ActionController::TestCase
 
   def test_should_create_<%= file_name %>
     assert_difference('<%= class_name %>.count') do
-      post :create, :<%= file_name %> => { }
+      xhr :post, :create, :format => 'ext_json', :<%= file_name %> => { }
     end
 
-    assert_redirected_to <%= file_name %>_path(assigns(:<%= file_name %>))
+    assert flash[:notice]
+    assert_response :success
   end
 
   def test_should_show_<%= file_name %>
@@ -31,15 +38,16 @@ class <%= controller_class_name %>ControllerTest < ActionController::TestCase
   end
 
   def test_should_update_<%= file_name %>
-    put :update, :id => <%= table_name %>(:one).id, :<%= file_name %> => { }
-    assert_redirected_to <%= file_name %>_path(assigns(:<%= file_name %>))
+    xhr :put, :update, :format => 'ext_json', :id => <%= table_name %>(:one).id, :<%= file_name %> => { }
+    assert flash[:notice]
+    assert_response :success
   end
 
   def test_should_destroy_<%= file_name %>
     assert_difference('<%= class_name %>.count', -1) do
-      delete :destroy, :id => <%= table_name %>(:one).id
+      xhr :delete, :destroy, :id => <%= table_name %>(:one).id
     end
 
-    assert_redirected_to <%= table_name %>_path
+    assert_response :success
   end
 end
