ed4a73fd1500be9d195f443d177af3178647bc82
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-10-12 22:25:16 +0200

    updated functional test template

--- generators/ext_scaffold/templates/functional_test.rb
+++ generators/ext_scaffold/templates/functional_test.rb
@@ -17,33 +17,15 @@ class <%= controller_class_name %>ControllerTest < ActionController::TestCase
     assert_not_nil assigns(:<%= table_name %>)
   end
 
-  def test_should_get_new
-    get :new
-    assert_response :success
-  end
-
   def test_should_create_<%= file_name %>
     assert_difference('<%= class_name %>.count') do
       xhr :post, :create, :format => 'ext_json', :<%= file_name %> => { }
     end
-
-    assert_not_nil flash[:notice]
-    assert_response :success
-  end
-
-  def test_should_show_<%= file_name %>
-    get :show, :id => <%= table_name %>(:one).id
-    assert_response :success
-  end
-
-  def test_should_get_edit
-    get :edit, :id => <%= table_name %>(:one).id
     assert_response :success
   end
 
   def test_should_update_<%= file_name %>
     xhr :put, :update, :format => 'ext_json', :id => <%= table_name %>(:one).id, :<%= file_name %> => { }
-    assert_not_nil flash[:notice]
     assert_response :success
   end
 
@@ -51,7 +33,6 @@ class <%= controller_class_name %>ControllerTest < ActionController::TestCase
     assert_difference('<%= class_name %>.count', -1) do
       xhr :delete, :destroy, :id => <%= table_name %>(:one).id
     end
-
     assert_response :success
   end
 end
