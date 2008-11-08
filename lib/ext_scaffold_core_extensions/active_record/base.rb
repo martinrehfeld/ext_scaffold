f03ee9f2a02ac33010458a5c60f951667a8fc323
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-11-08 09:27:49 +0100

    support methods option in ActiveRecord::Base#to_ext_json

--- lib/ext_scaffold_core_extensions/active_record/base.rb
+++ lib/ext_scaffold_core_extensions/active_record/base.rb
@@ -4,19 +4,26 @@ module ExtScaffoldCoreExtensions
 
       def to_ext_json(options = {})
         success = options.delete(:success)
+        methods = Array(options.delete(:methods))
+        underscored_class_name = self.class.to_s.demodulize.underscore
+
         if success || (success.nil? && valid?)
-          # return sucess/data hash to form loader, i.e.:
+          # return success/data hash to form loader, i.e.:
           #  {"data": { "post[id]": 1, "post[title]": "First Post",
           #             "post[body]": "This is my first post.",
           #             "post[published]": true, ...},
           #   "success": true}
-          { :success => true, :data => Hash[*attributes.map{|name,value| ["#{self.class.to_s.demodulize.underscore}[#{name}]", value] }.flatten] }.to_json(options)
+          data =  attributes.map{|name,value| ["#{underscored_class_name}[#{name}]", value] }
+          methods.each do |method|
+            data << ["#{underscored_class_name}[#{method}]", self.send(method)] if self.respond_to? method
+          end
+          { :success => true, :data => Hash[*data.flatten] }.to_json(options)
         else
-          # return no-sucess/errors hash to form submitter, i.e.:
+          # return no-success/errors hash to form submitter, i.e.:
           #  {"errors":  { "post[title]": "Title can't be blank", ... },
           #   "success": false }
           error_hash = errors.inject({}) do |result, error| # error is [attribute, message]
-            field_key = "#{self.class.to_s.demodulize.underscore}[#{error.first}]"
+            field_key = "#{underscored_class_name}[#{error.first}]"
             result[field_key] ||= 'Field ' + Array(errors[error.first]).join(' and ')
             result
           end
