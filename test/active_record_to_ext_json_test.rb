e64bc0dcf8bd6df2e9feb7e02599c3610d7d783a
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-12-04 13:28:10 +0100

    add test for ActiveRecord::Base#to_ext_json

--- test/active_record_to_ext_json_test.rb
+++ test/active_record_to_ext_json_test.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/test_helper'
+require 'test/unit'
 
 class StubModel < ActiveRecord::Base
   def self.columns # make AR happy even without existing DB table
@@ -9,5 +10,26 @@ end
 class ActiveRecordToExtJsonTest < Test::Unit::TestCase
   def test_to_ext_json_should_be_defined
     assert StubModel.new.respond_to?(:to_ext_json)
+  end
+  
+  def test_generate_ext_json_with_valid_model
+    assert_equal({'success' => true, 'data' => {}},ActiveSupport::JSON::decode(StubModel.new.to_ext_json))
+  end
+
+  def test_generate_ext_json_with_invalid_model
+    stub_model = StubModel.new
+    def stub_model.valid?
+      false
+    end
+    assert_equal({'success' => false, 'errors' => {}},ActiveSupport::JSON::decode(stub_model.to_ext_json))
+  end
+
+  def test_generate_ext_json_with_additional_methods
+    stub_model = StubModel.new
+    def stub_model.my_method
+      "my_value"
+    end
+    assert_equal({'success' => true, 'data' => {'stub_model[my_method]' => 'my_value'}},
+                 ActiveSupport::JSON::decode(stub_model.to_ext_json(:methods => :my_method)))
   end
 end
