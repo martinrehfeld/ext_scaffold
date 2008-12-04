7afaaef2467155031b4edc3a611f927d15cd0503
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-12-04 13:14:21 +0100

    add test for Array#to_ext_json

--- test/array_to_ext_json_test.rb
+++ test/array_to_ext_json_test.rb
@@ -1,7 +1,26 @@
 require File.dirname(__FILE__) + '/test_helper'
+require 'test/unit'
+
+class ProclaimedKlazz; end
 
 class ArrayToExtJsonTest < Test::Unit::TestCase
   def test_to_ext_json_should_be_defined
     assert Array.new.respond_to?(:to_ext_json)
   end
+
+  def test_generate_ext_json_with_derived_class
+    data = [ [1], [2] ]
+    assert_equal({'results' => data.size, 'arrays' => data}, ActiveSupport::JSON::decode(data.to_ext_json))
+  end
+  
+  def test_generate_ext_json_with_given_class
+    data = [ [1], [2] ]
+    assert_equal({'results' => data.size, 'proclaimed_klazzs' => data}, ActiveSupport::JSON::decode(data.to_ext_json(:class => 'proclaimed_klazz')))
+  end
+
+  def test_generate_ext_json_with_given_count
+    data = [ [1], [2] ]
+    assert_equal({'results' => 5, 'arrays' => data}, ActiveSupport::JSON::decode(data.to_ext_json(:count => 5)))
+  end
+
 end
