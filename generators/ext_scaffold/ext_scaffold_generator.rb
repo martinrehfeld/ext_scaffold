ac574ff8f7f4e6a832dc009a028b4c151006ef26
Jonathan Barket <jbarket@jonathan-barkets-macbook.local>
2009-02-11 13:03:20 -0600

    Moved assets into template files and made them copy over automatically. Added rspec detection and very basic rspec test skeletons. Modified layout to use complete paths to ext rather than relative paths from /javascripts/

--- generators/ext_scaffold/ext_scaffold_generator.rb
+++ generators/ext_scaffold/ext_scaffold_generator.rb
@@ -17,6 +17,8 @@ class ExtScaffoldGenerator < Rails::Generator::NamedBase
   def initialize(runtime_args, runtime_options = {})
     super
 
+    @rspec = has_rspec?
+    
     @controller_name = @name.pluralize
 
     base_name, @controller_class_path, @controller_file_path, @controller_class_nesting, @controller_class_nesting_depth = extract_modules(@controller_name)
@@ -41,11 +43,20 @@ class ExtScaffoldGenerator < Rails::Generator::NamedBase
       m.directory(File.join('app/helpers', controller_class_path))
       m.directory(File.join('app/views', controller_class_path, controller_file_name))
       m.directory(File.join('app/views/layouts', controller_class_path))
-      m.directory(File.join('test/functional', controller_class_path))
-      m.directory(File.join('test/unit', class_path))
       m.directory('public/images/ext_scaffold')
       m.directory(File.join('public/javascripts/ext_scaffold', "#{controller_class_path}"))
 
+      if @rspec
+        m.directory(File.join('spec/controllers', controller_class_path))
+        m.directory(File.join('spec/helpers', class_path))
+        m.directory(File.join('spec/models', class_path))
+        m.directory File.join('spec/views', controller_class_path, controller_file_name)
+        m.directory(File.join('spec/fixtures', class_path))
+      else
+        m.directory(File.join('test/functional', controller_class_path))
+        m.directory(File.join('test/unit', class_path))
+      end
+            
       # index view
       m.template('view_index.html.erb', File.join('app/views', controller_class_path, controller_file_name, 'index.html.erb'))
       
@@ -60,20 +71,42 @@ class ExtScaffoldGenerator < Rails::Generator::NamedBase
 
       # controller
       m.template('controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb"))
+      m.template('helper.rb',          File.join('app/helpers',     controller_class_path, "#{controller_file_name}_helper.rb"))
+
+      # assets
+      m.template('assets/javascripts/ext_scaffold.js', 'public/javascripts/ext_scaffold.js')
+      m.template('assets/images/arrowLeft.gif', 'public/images/ext_scaffold/arrowLeft.gif')
+      m.template('assets/images/arrowRight.gif', 'public/images/ext_scaffold/arrowRight.gif')
+      m.template('assets/stylesheets/ext_scaffold.css', 'public/stylesheets/ext_scaffold.css')
 
       # tests
-      m.template('functional_test.rb', File.join('test/functional', controller_class_path, "#{controller_file_name}_controller_test.rb"))
-      m.template('helper.rb',          File.join('app/helpers',     controller_class_path, "#{controller_file_name}_helper.rb"))
+      if @rspec
+        m.template('rspec/functional_spec.rb', File.join('spec/controllers', controller_class_path, "#{controller_file_name}_controller_spec.rb"))
+        m.template('rspec/routing_spec.rb', File.join('spec/controllers', controller_class_path, "#{controller_file_name}_routing_spec.rb"))
+        m.template('rspec/helper_spec.rb', File.join('spec/helpers', class_path, "#{controller_file_name}_helper_spec.rb"))
+        m.template('rspec/unit_spec.rb', File.join('spec/models', class_path, "#{file_name}_spec.rb"))
+      else
+        m.template('functional_test.rb', File.join('test/functional', controller_class_path, "#{controller_file_name}_controller_test.rb"))
+      end
 
       m.route_resources controller_file_name
     end
   end
 
+  # Lifted from Rick Olson's restful_authentication
+  def has_rspec?
+    options[:rspec] || (File.exist?('spec') && File.directory?('spec'))
+  end
+  
   protected
     # Override with your own usage banner.
     def banner
       "Usage: #{$0} ext_scaffold ModelName [field:type, field:type]"
     end
+    
+    def rspec_views
+      %w[ index show ]
+    end    
 
     def add_options!(opt)
       opt.separator ''
@@ -82,6 +115,8 @@ class ExtScaffoldGenerator < Rails::Generator::NamedBase
              "Don't add timestamps to the migration file for this model") { |v| options[:skip_timestamps] = v }
       opt.on("--skip-migration",
              "Don't generate a migration file for this model") { |v| options[:skip_migration] = v }
+      opt.on("--rspec", 
+             "Force rspec mode (checks for RAILS_ROOT/spec by default)") { |v| options[:rspec] = true }
     end
 
     def model_name
